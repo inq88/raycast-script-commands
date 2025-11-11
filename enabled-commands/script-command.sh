8caf5950672522a90cd49c028570bf555015321f
Bob Richters <19558681+inq88@users.noreply.github.com>
2025-11-11 22:16:26 +0100

    Adds example script command
    Introduces an example script command to demonstrate the basic structure and required parameters for Raycast script commands.
    
    This serves as a starting point for users to create their own custom commands.

---
+++ enabled-commands/script-command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title My First Script
+# @raycast.mode fullOutput
+# @raycast.packageName Raycast Scripts
+#
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Write a nice and descriptive summary about your script command here
+# @raycast.author Your name
+# @raycast.authorURL An URL for one of your social medias
+
+echo "Hello from My First Script"
